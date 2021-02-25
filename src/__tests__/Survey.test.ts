2907b44da96316d8e03a9e21236d04cfd855a75a
Mikael Boff <mikaelboffdev@gmail.com>
2021-02-25 02:50:40 -0300

    Aula 3: Refatorações, Repository e Teste de integração
     - Configuração do Jest, criação de teste de integração para User e Survey, refatoração para utilizar repository custom de cada modelo.

---
+++ src/__tests__/Survey.test.ts
@@ -0,0 +1,37 @@
+import request from "supertest";
+import { app } from "../app";
+import createConnection from "../database";
+
+describe("Users", () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Should be able to create a new survey", async () => {
+    const response = await request(app)
+      .post("/surveys")
+      .send({
+        title: "Campeonato brasileiro",
+        description: "Qual a probabilidade do inter ganhar o brasileirao?"
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("Should be able to get all surveys", async () => {
+    await request(app)
+      .post("/surveys")
+      .send({
+        title: "Campeonato brasileiro",
+        description: "Qual a probabilidade do inter perder o brasileirao?"
+      });
+
+    const response = await request(app)
+      .get("/surveys")
+      .send();
+
+    expect(response.body).toHaveLength(2);
+  });
+});
